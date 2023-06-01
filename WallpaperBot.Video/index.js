2ce30852181ac4d23e0ec01040e86b162851f579
siuramka <marius.kamarauskas2001@gmail.com>
2023-06-01 15:05:15 +0300

    write text to image, linux subsystem

--- WallpaperBot.Video/index.js
+++ WallpaperBot.Video/index.js
@@ -1,3 +1,6 @@
+//for linux
+//writtein in linux wsl subsystem
+// should work in docker copium
 const fs = require('fs')
 const path = require('path')
 const intro_images_dir_path = path.join(__dirname, 'storage/intro_images')
@@ -8,13 +11,13 @@ const { exec } = require('child_process');
 function cropImageToIphoneSize(image) {
     const imagename = image.split('.')[0]
     const imagePath = intro_images_dir_path + "/" +image
-    const outputImagePath = intro_images_dir_path + `/output_${imagename}.jpeg`
+    const outputImagePath = intro_images_dir_path + `/cropped_${imagename}.jpeg`
     const command = `ffmpeg -i ${imagePath} -vf "scale=1300:2800:force_original_aspect_ratio=increase,crop=1300:2800" -qscale:v 1 ${outputImagePath}`;
     executeFfmpegCommand(command);
 }
 
-function createTextFile(text) {
-    fs.writeFile(intro_images_text_dir_path + '/text.txt', text, (err) => {
+function createTextFile(text, name) {
+    fs.writeFile(intro_images_text_dir_path + `/${name}.txt`, text, (err) => {
         if (err) throw err;
         console.log('Text file created!');
       });
@@ -29,11 +32,25 @@ function executeFfmpegCommand(command) {
         console.log('Image conversion complete!');
       });
 }
-createTextFile("afasf\nasfasfas\nasfdasd")
+
+function writeTextToImage(image, textFile) {
+    const imagename = image.split('.')[0]
+    const croppedImagePath = intro_images_dir_path + `/` + `cropped_${imagename}.jpeg`
+    const textFilePath = intro_images_text_dir_path + `/` + `${textFile}`
+    const command = `ffmpeg -i ${croppedImagePath} -vf "drawtext=textfile=${textFilePath}:fontsize=0.1*W:fontcolor=white:x=0.1*W:y=0.3*H:bordercolor=black:borderw=15" -q:v 1 ${intro_images_dir_path}/final_${image}`;
+    executeFfmpegCommand(command);
+}
+// createTextFile("text","text1")
+writeTextToImage("image1.jpeg","text.txt")
+
 // fs.readdir(intro_images_dir_path, (error, files) => {
 //   if (error) console.log(error);
 //   files.forEach((file) => {
-//     cropImageToIphoneSize(file)
-//   })
+//     try {
+//       cropImageToIphoneSize(file);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   });
 // });
 
