241b2b6029e260a1afd33eb796a4578d51246e83
siuramka <marius.kamarauskas2001@gmail.com>
2023-06-02 14:35:04 +0300

    wallpaper slideshow vid

--- WallpaperBot.Video/index.js
+++ WallpaperBot.Video/index.js
@@ -1,16 +1,19 @@
 //for linux
 //writtein in linux wsl subsystem
 // should work in docker copium
+const util = require("util");
 const fs = require("fs");
 const path = require("path");
 const intro_images_dir_path = path.join(__dirname, "storage/intro_images");
 const intro_video_output_dir = path.join(__dirname, "storage/pipe/intro");
+const slideshow_video_output_dir = path.join(__dirname, "storage/pipe/slideshow");
+const slideshow_wallpapers_dir = path.join(__dirname, "storage/wallpapers");
+const { exec } = require("child_process");
 
 const intro_images_text_dir_path = path.join(
     __dirname,
     "storage/intro_images_text"
 );
-const { exec } = require("child_process");
 
 async function cropImageToIphoneSize(image, dir) {
     const imagename = image.split(".")[0];
@@ -49,20 +52,30 @@ async function writeTextToImage(image, textFile) {
     await executeFfmpegCommand(command);
 }
 
-function duplicateImageOverlay(image) {
+async function duplicateImageOverlay(image) {
     const imagename = image.split(".")[0];
-    const finalImagePath = wallpapers_dir + `/` + `cropped_${imagename}.jpeg`;
-    const outputImagePath = wallpapers_dir + `/overlayed_${imagename}.jpeg`;
-    const command = `ffmpeg -i ${finalImagePath} -i ${finalImagePath} -filter_complex "[0:v]eq=gamma=1.7[gamma_corrected]; [1:v]scale=iw*0.8:ih*0.8[scaled_overlay]; [gamma_corrected][scaled_overlay]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -qscale:v 2 ${outputImagePath}`;
-    executeFfmpegCommand(command);
+    const inputPath =
+        slideshow_wallpapers_dir + `/cropped` + `/cropped_${imagename}.jpeg`;
+    const outputImagePath =
+        slideshow_wallpapers_dir + `/final` + `/${imagename}.jpeg`;
+    const command = `ffmpeg -i ${inputPath} -i ${inputPath} -filter_complex "[0:v]eq=gamma=1.7[gamma_corrected]; [1:v]scale=iw*0.8:ih*0.8[scaled_overlay]; [gamma_corrected][scaled_overlay]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -qscale:v 2 ${outputImagePath}`;
+    await executeFfmpegCommand(command);
 }
-const wallpapers_dir = path.join(__dirname, "storage/wallpapers");
 
 //image or single images
 async function convertImageToVideo(imagePath, images, outputPath, outputName) {
     const output = outputPath + "/video/" + outputName;
     const command = `ffmpeg -r 1/5 -i ${imagePath + "/" + images
         } -c:v libx264 -pix_fmt yuv422p ${output}.mp4`;
+    await executeFfmpegCommand(command);
+}
+
+async function imagesToSlideshowVideo(imagesFileFormat) {
+    // image%d - format: image[number]
+    console.log("Rendering slideshow...")
+    const inputPath = slideshow_wallpapers_dir + `/final`
+    const randomName = (Math.random() + 1).toString(36).substring(7);
+    const command = `ffmpeg -r 1/2 -pattern_type glob -i '${inputPath}/*.jpeg' -r 25 -c:v libx264 -pix_fmt yuv422p ${slideshow_video_output_dir}/slideshow_${randomName}.mp4`
     await executeFfmpegCommand(command);
 }
 
@@ -86,7 +99,6 @@ async function introPipeOne() {
 }
 
 async function introPipeTwo() {
-    console.log("here");
     const inputPath = intro_images_dir_path + "/with_text";
     const files = await fs.promises.readdir(inputPath);
 
@@ -112,15 +124,36 @@ async function introPipeline() {
     await introPipeOne();
     await introPipeTwo();
     console.log("intro pipeline completed");
+}
 
+async function slideshowPipeOne() {
+    const files = await fs.promises.readdir(slideshow_wallpapers_dir);
+    for (const file of files) {
+        if (file.split(".").length > 1) {
+            console.log(file);
+            try {
+                await cropImageToIphoneSize(file, slideshow_wallpapers_dir);
+                await duplicateImageOverlay(file);
+            } catch (e) {
+                console.err(e);
+            }
+        }
+    }
 }
 
 async function run() {
-    await introPipeline();
+    await slideshowPipeOne();
+    await imagesToSlideshowVideo("overlayedimage%03d")
 }
-
 // need to fix or just add -y  flag to ffmpeg commands
 // to remove bug, if output image already exists, it waits for user command(freezezs theexecution ;9)
 
-run();
+(async () => {
+    try {
+        const text = await run();
+    } catch (e) {
+        // Deal with the fact the chain failed
+    }
+    // `text` is not available here
+})();
 // duplicateImageOverlay("image4.jpeg")
